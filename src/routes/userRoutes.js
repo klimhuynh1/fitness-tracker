1839aba87ce2a81d5d3c4c40d48d69823519183e
Klim Huynh <klimhuynh@gmail.com>
2024-08-23 17:27:30 +1000

    feat(user): add routes for single user operations
    - Implemented route to get a single user by ID
    - Added route to update user details
    - Created route to delete a user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -24,4 +24,56 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Get a single user
+router.get("/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Update user
+router.patch("/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const updates = req.body;
+
+    const updatedUser = await User.findByIdAndUpdate(userId, updates, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({ message: "User updated successfully", updatedUser });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Delete user
+router.delete("/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    const deletedUser = await User.findByIdAndDelete(userId);
+
+    if (!deletedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.status(200).json({ message: "User deleted sucessfully" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
 module.exports = router;
