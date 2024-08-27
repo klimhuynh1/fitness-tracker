918e7ddc0bdfb6f785b7c8c28eeb14c5f617f862
Klim Huynh <klimhuynh@gmail.com>
2024-08-27 15:16:12 +1000

    chore: move exercise ID validation middleware to separate file

---
+++ src/middleware/validateExerciseIds.js
@@ -0,0 +1,19 @@
+// Middleware function to validate exercise IDs inthe request
+const validateExerciseIds = async (req, res, next) => {
+  const { exercises } = req.body;
+
+  if (exercises) {
+    for (const exercise of exercises) {
+      const validExercise = await Exercise.findById(exercise.exerciseId);
+      if (!validExercise) {
+        return res
+          .status(400)
+          .json({ error: `Exercise with ID ${exercise.exerciseId} not found` });
+      }
+    }
+  }
+
+  next();
+};
+
+module.exports = validateExerciseIds;
--- src/routes/workoutRoutes.js
+++ src/routes/workoutRoutes.js
@@ -2,24 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Workout = require("../models/Workout");
 const Exercise = require("../models/Exercise");
-
-// Middleware function to validate exercise IDs inthe request
-const validateExerciseIds = async (req, res, next) => {
-  const { exercises } = req.body;
-
-  if (exercises) {
-    for (const exercise of exercises) {
-      const validExercise = await Exercise.findById(exercise.exerciseId);
-      if (!validExercise) {
-        return res
-          .status(400)
-          .json({ error: `Exercise with ID ${exercise.exerciseId} not found` });
-      }
-    }
-  }
-
-  next();
-};
+const validateExerciseIds = require("../middleware/validateExerciseIds");
 
 // Create a new workout
 router.post("/", async (req, res) => {
